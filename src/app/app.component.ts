2ef4e187f927588e3058adcbbfa0f6a019b14588
Pedro Solorzano <pedro1991luis@gmail.com>
2024-08-13 23:36:02 +0200

    Primera versión de mi proyecto Angular Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavegationComponent } from "./navegation/navegation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavegationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
